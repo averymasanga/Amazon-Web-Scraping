7c6e5bf202d7baf88d9b36fd48cbea7e7c97664f
Avery Masanga <124748653+averymasanga@users.noreply.github.com>
2023-08-07 15:12:02 -0700

    Add files via upload

---
+++ Amazon Web Scraping.py
@@ -0,0 +1,131 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+import datetime
+
+import smtplib
+
+
+# Connect to Website and pull in data
+
+URL = 'https://www.amazon.com/Funny-Data-Systems-Business-Analyst/dp/B07FNW9FGJ/ref=sr_1_3?dchild=1&keywords=data%2Banalyst%2Btshirt&qid=1626655184&sr=8-3&customId=B0752XJYNL&th=1'
+
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+
+page = requests.get(URL, headers=headers)
+
+soup1 = BeautifulSoup(page.content, "html.parser")
+
+soup2 = BeautifulSoup(soup1.prettify(), "html.parser")
+
+title = soup2.find(id='productTitle').get_text()
+
+price = soup2.find(id='corePriceDisplay_desktop_feature_div').get_text()
+
+
+# Clean up the data a little bit
+
+price = price.strip()[:6]
+title = title.strip()
+
+print(title)
+print(price)
+
+
+# Create a Timestamp for your output to track when data was collected
+
+import datetime
+
+today = datetime.date.today()
+
+print(today)
+
+
+# Create CSV and write headers and data into the file
+
+import csv 
+
+header = ['Title', 'Price', 'Date']
+data = [title, price, today]
+
+with open('AmazonWebScraperDataset.csv', 'w', newline='', encoding='UTF8') as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
+    writer.writerow(data)
+    
+import pandas as pd
+
+df = pd.read_csv(r'C:\Users\avery\Desktop\Projects\Portfolio Projects\Amazon Web Scraping Project\AmazonWebScraperDataset.csv')
+
+print(df)
+
+
+# Appending data to the csv
+
+with open('AmazonWebScraperDataset.csv', 'a+', newline='', encoding='UTF8') as f:
+    writer = csv.writer(f)
+    writer.writerow(data)
+
+
+# Combine all of the above code into one function
+
+def check_price():
+    URL = 'https://www.amazon.com/Funny-Data-Systems-Business-Analyst/dp/B07FNW9FGJ/ref=sr_1_3?dchild=1&keywords=data%2Banalyst%2Btshirt&qid=1626655184&sr=8-3&customId=B0752XJYNL&th=1'
+
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+
+    page = requests.get(URL, headers=headers)
+
+    soup1 = BeautifulSoup(page.content, "html.parser")
+
+    soup2 = BeautifulSoup(soup1.prettify(), "html.parser")
+
+    title = soup2.find(id='productTitle').get_text()
+
+    price = soup2.find(id='corePriceDisplay_desktop_feature_div').get_text()
+
+    price = price.strip()[:6]
+    title = title.strip()
+
+    import datetime
+
+    today = datetime.date.today()
+    
+    import csv 
+
+    header = ['Title', 'Price', 'Date']
+    data = [title, price, today]
+
+    with open('AmazonWebScraperDataset.csv', 'a+', newline='', encoding='UTF8') as f:
+        writer = csv.writer(f)
+        writer.writerow(data)
+
+    if(price < 14):
+        send_mail()
+
+import pandas as pd
+
+df = pd.read_csv(r'C:\Users\avery\Desktop\Projects\Portfolio Projects\Amazon Web Scraping Project\AmazonWebScraperDataset.csv')
+
+print(df)
+
+
+# Send email to self
+
+def send_mail():
+    server = smtplib.SMTP_SSL('smtp.gmail.com',465)
+    server.ehlo()
+    #server.starttls()
+    server.ehlo()
+    server.login('averymasanga@gmail.com','xxxxxxxxxx')
+    
+    subject = "The Shirt you want is below $15! Now is your chance to buy!"
+    body = "Avery, This is the moment we have been waiting for. Now is your chance to pick up the shirt of your dreams. Don't mess it up! Link here: https://www.amazon.com/Funny-Data-Systems-Business-Analyst/dp/B07FNW9FGJ/ref=sr_1_3?dchild=1&keywords=data+analyst+tshirt&qid=1626655184&sr=8-3"
+   
+    msg = f"Subject: {subject}\n\n{body}"
+    
+    server.sendmail(
+        'averymasanga@gmail.com',
+        msg
+
+    )
